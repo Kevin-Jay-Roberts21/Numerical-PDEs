f16d1c6eec80477674267f43e5277d620e103088
Kevin-Jay-Roberts21 <kevinjrob9@gmail.com>
2024-09-27 09:53:09 -0600

    Going to talk to Jim about what the issue is here.

--- third_order_upwind_vNeumann.m
+++ third_order_upwind_vNeumann.m
@@ -2,17 +2,6 @@ clear all
 close all
 clc
 
-% 
-% Script performing stability/phase error analysis for a third-order
-% accurate approach to solving the advection equation, u_t + c u_x =0.  The
-% third order scheme is
-% 
-%   u(j,n+1) = u(j,n) -1/2 phi ( u(j+1,n) - u(j-1,n) )
-%                   +1/2 phi^2 (u(j+1,n) + u(j-1,n) -2 u(j,n) )
-% 
-
-% Just for definiteness, assume we are working on an interval of length 10
-% with 100 subdivisions:
 L=10; J=100; dx=L/J; c=1;
 
 % define a vector of wave numbers
@@ -20,8 +9,7 @@ khat=2*pi*[1:J/2]/J;    % equivalent to 2*pi*k/L*dx
 
 % define a vector of phi to test
 nphi=50; phi=linspace(1/nphi,2,nphi); % we start at a small phi rather than zero 
-                        % to avoid division by zero below
-
+                        
 % define a 2D grid of k, phi for density plotting
 [K,Phi]=meshgrid(khat,phi);
 
@@ -34,12 +22,12 @@ a_1 = 1/6*(-2*Phi + 3*Phi^2 - Phi^3);
 A1 = a_1*exp(i*K) + a_0 + a_neg1*exp(-i*K) + a_neg2*exp(-2*i*K);
 
 % calculate the (complex) von Neumann multiplier Lax-Wendroff
-A2 = 1 -.5*Phi.*(exp(i*K) -exp(-i*K)) ...
+A1 = 1 -.5*Phi.*(exp(i*K) -exp(-i*K)) ...
     +.5*Phi.^2.*(exp(i*K) +exp(-i*K) -2);
 
 
 Lambda=abs(A1);
-
+x
 % plot results as a hot density plot, with an additional (green) contour 
 % indicating lambda = 1
 pcolor(K/dx,Phi,Lambda), shading flat, colormap hot, colorbar
