d09cf791c1c0ead19dfd6d2a202e7ed98d8e041a
Kevin-Jay-Roberts21 <kevinjrob9@gmail.com>
2024-12-06 12:15:37 -0700

    Reverted back to using the original tri_diag_sol. Still working out BC.

--- crank_on_heat.m
+++ crank_on_heat.m
@@ -2,12 +2,15 @@
 % Kevin Roberts
 % October 2024
 
+
+
+% FOR HOMEOGENOUS DIRCHLET BC
+%%
+
 clear all
 close all
 clc
 
-% FOR HOMEOGENOUS DIRCHLET BC
-%%
 % Set the parameters for the solution
 L = 20; % length of solution domain (from zero)
 J = 200; % number of grid points (not counting zero)
@@ -68,8 +71,8 @@ for n = 1:N
     end
     d(end) = off_diagB(end)*u_inner(end-1) + diagB(end)*u_inner(end);
     
-    FTCS(2:end-1) = tri_diag_sol(a, b, c, d, b0, bL); % updating FTCSn (the solution at time step n)
-    
+    FTCS(2:end-1) = tri_diag_sol(a, b, c, d); 
+
     % apply the boundary conditions
     FTCS(1) = b0;
     FTCS(end) = bL;
@@ -88,140 +91,172 @@ for n = 1:N
     FTCS = FTCSn;
 end
 
-
 % FOR BC u(0, t) = 0 and u(L, t) = 2
 %%
 % Parameters
-L = 1; % Length of the spatial domain
-J = 50; % Number of spatial points
-x = linspace(0, L, J+1)'; % space grid
-dx = L/J; % Spatial step size
-D = 1; % Diffusion coefficient
-dt = T/N; % Time step size
-p = D*dt/dx^2;  % Diffusion parameter (stability)
-T = 0.1; % Total time
-N = 1000; % Number of time steps
+clear all
+close all
+clc
 
-% Initial condition
-u = zeros(N+1, J+1);
-u(1, :) = 2*sin(pi*x/L) + sin(2*pi*x/L);
+% Set the parameters for the solution
+L = 20; % length of solution domain (from zero)
+J = 200; % number of grid points (not counting zero)
+x = linspace(0, L, J+1); % set up a vector of x locations
+dx = L/J; % size of a grid cell, delta x
+D = 1; % diffusion coefficient
+dt = 0.0016; % size of time step, delta t
+p = D*dt/(dx^2); % defining the dimensionless combination to be p (rho)
+T = 15; % duraction of simulation
+N = round(T/dt); % number of times to iterate simuation to get to time T
 
-% Boundary conditions
-u(:,1) = 0; % u(0, t) = 0
-u(:,end) = 2; % u(L, t) = 2
+% modifying the boundary conditions
+b0 = 0; % left boundary condition
+bL = 2; % right boundary condition
 
-% Coefficients for the tridiagonal matrix
-a = (1 + 2*p)*ones(J-1, 1); % Main diagonal
-b = -p*ones(J-2, 1); % lower off diagonal
-c = -p*ones(J-2, 1); % upper off diagonal
+% Used for homogeneous bc
+i_c = 2*sin(x*(pi/L)) + sin(x*(2*pi/L));
 
-plot(x, u(1, :), 'r');
-% Time-stepping loop
+% setting the initial conditions for the various methods
+exact = i_c;
+FTCS = i_c';
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% FTCS Method on the Diffusion Equation %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+hold on, plot(x, FTCS, 'r'), hold off % plot the IC
+
+% add labels to the plot
+xlabel('x'), ylabel('u(x,t)')
+title(['Crank Nicolson method on the diffusion equation, \rho = ' num2str(D*dt/(dx^2))])
+
+% Define matrices A and B
+diagA = (1 + p)*ones(J+1, 1); % diagonal
+off_diagA = (-p/2)*ones(J, 1); % off diagonals
+A = diag(diagA) + diag(off_diagA, 1) + diag(off_diagA, -1);
+
+diagB = (1 - p)*ones(J+1, 1); % diagonal
+sup_diagB = (p/2)*ones(J, 1); % super off diagonals
+sub_diagB = (p/2)*ones(J, 1); % sub off diagonals
+
+% account for boundary conditions (need to change the top and bottom row of A and B)
+A(1, 1) = 1;
+A(1, 2) = 0;
+A(J+1, J+1) = 1;
+A(J+1, J) = 0;
+
+diagB(1) = 1;
+diagB(J+1) = p;
+sup_diagB(1) = 0;
+sub_diagB(J) = 0;
+
+B = diag(diagB) + diag(sup_diagB, 1) + diag(sub_diagB, -1);
+
+% defining vectors that will be plugged into the tridiagonal function
+a = diagA;
+b = off_diagA;
+c = off_diagA;
+
 for n = 1:N
-    % Construct RHS vector
-    d = p*u(n, 1:J-1)' + (1 - 2*p)*u(n, 2:J)' + p*u(n, 3:J+1)';
-    d(1) = d(1) + p*u(n+1, 1); % BC at x = 0
-    d(end) = d(end) + p*u(n+1, end); % BC at x = L
     
-    % Solve using your tridiagonal solver
-    u(n+1, 2:J) = tri_diag_sol(a, b, c, d, b0, bL);
+    t = n*dt;
+
+    % Compute d using the tridiagonal structure (as opposed to d = B * FTCS(2:end-1))
+    
+    d(1) = b0; % diagB(1)*FTCS(2) + sup_diagB(1)*FTCS(1);
+    for j = 2:J
+        d(j) = sup_diagB(j-1)*FTCS(j-1) + diagB(j)*FTCS(j) + sub_diagB(j-1)*FTCS(j+1);
+    end
+    d(J+1) = bL; % sub_diagB(J)*FTCS(J) + diagB(J+1)*FTCS(J+1);
+    
+    FTCSn = tri_diag_sol(a, b, c, d); 
+
+    if mod(n*dt,2) < dt % check if current time is close to a multiple of 2
+        hold on, plot(x,FTCSn,'b'), hold off % if so, add a plot of current solution to existing plot
+    end
+
+    % Update for the next time step
+    FTCS = FTCSn;
 end
-
-% Plotting the results
-hold on;
-for i = 10:N/10:N+1
-    hold on, plot(x, u(i, :), 'b'), hold off;
-end
-xlabel('x');
-ylabel('u(x, t)');
-title('Crank-Nicolson Solution of the Diffusion Equation');
-
-
 
 
 % FOR NO FLUX BC at x=L
 %%
 % Parameters
-L = 1; % Length of the spatial domain
-J = 50; % Number of spatial points
-x = linspace(0, L, J+1)'; % Space grid
-dx = L/J; % Spatial step size
-D = 1; % Diffusion coefficient
-T = 0.1; % Total time
-N = 1000; % Number of time steps
-dt = T/N; % Time step size
-p = D*dt/dx^2;  % Diffusion parameter (stability)
+clear all
+close all
+clc
 
-% Initial condition
-u = zeros(N+1, J+1);
-u(1, :) = 2*sin(pi*x/(2*L)) + sin(3*pi*x/(2*L));
+% Set the parameters for the solution
+L = 20; % length of solution domain (from zero)
+J = 200; % number of grid points (not counting zero)
+x = linspace(0, L, J+1); % set up a vector of x locations
+dx = L/J; % size of a grid cell, delta x
+D = 1; % diffusion coefficient
+dt = 0.0016; % size of time step, delta t
+p = D*dt/(dx^2); % defining the dimensionless combination to be p (rho)
+T = 15; % duraction of simulation
+N = round(T/dt); % number of times to iterate simuation to get to time T
 
-% Boundary conditions
-u(:, 1) = 0; % u(0, t) = 0
+% Used for homogeneous bc
+i_c = 2*sin(x*(pi/(2*L))) + 3*sin(x*(3*pi/(2*L)));
 
-% Coefficients for the tridiagonal matrix (Crank-Nicolson)
-a = (1 + 2*p)*ones(J-1, 1); % Main diagonal
-b = -p/2*ones(J-2, 1); % Lower off diagonal (modified for Crank-Nicolson)
-c = -p/2*ones(J-2, 1); % Upper off diagonal (modified for Crank-Nicolson)
+% setting the initial conditions for the various methods
+exact = i_c;
+FTCS = i_c';
 
-% Plot initial condition
-plot(x, u(1, :), 'r');
-hold on;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% FTCS Method on the Diffusion Equation %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+hold on, plot(x, FTCS, 'r'), hold off % plot the IC
 
-% Time-stepping loop
+% add labels to the plot
+xlabel('x'), ylabel('u(x,t)')
+title(['Crank Nicolson method on the diffusion equation, \rho = ' num2str(D*dt/(dx^2))])
+
+% Define matrices A and B
+diagA = (1 + p)*ones(J+1, 1); % diagonal
+off_diagA = (-p/2)*ones(J, 1); % off diagonals
+A = diag(diagA) + diag(off_diagA, 1) + diag(off_diagA, -1);
+
+diagB = (1 - p)*ones(J+1, 1); % diagonal
+sup_diagB = (p/2)*ones(J, 1); % super off diagonals
+sub_diagB = (p/2)*ones(J, 1); % sub off diagonals
+
+% account for boundary conditions (need to change the top and bottom row of A and B)
+% A(1, 1) = 1;
+% A(1, 2) = 0;
+%A(J+1, J+1) = 1;
+A(J+1, J) = p;
+
+% diagB(1) = 1;
+% diagB(J+1) = 1;
+% sup_diagB(1) = 0;
+%sub_diagB(J) = p;
+
+% defining vectors that will be plugged into the tridiagonal function
+a = diagA;
+b = off_diagA;
+c = off_diagA;
+
 for n = 1:N
-    % Prepare the right-hand side for Crank-Nicolson
-    rhs = (1 - p)*u(n, 2:J) + p/2*(u(n, 1:J-1) + u(n, 3:J+1)); % Right-hand side vector
     
-    % Adjust for boundary conditions at x=0 (Dirichlet)
-    rhs(1) = rhs(1) - b(1)*u(n, 1);  % Left boundary (Dirichlet or Neumann)
+    t = n*dt;
+
+    % Compute d using the tridiagonal structure (as opposed to d = B * FTCS(2:end-1))
     
-    % Adjust for no-flux boundary condition at x=L (centered difference approximation)
-    rhs(J-1) = rhs(J-1) - c(J-2)*u(n, J);  % Right boundary condition
-
-    % Solve the tridiagonal system using the given function
-    [u_new] = tri_diag_sol(a, b, c, rhs, u(n, 1), u(n, J));
-
-    % Update solution
-    u(n+1, 2:J) = u_new; % Updating interior points
-end
-
-% Plotting the results
-hold on;
-for i = 1:1:N+1
-    plot(x, u(i, :), 'b');
-end
-xlabel('x');
-ylabel('u(x, t)');
-title('Crank-Nicolson Solution of the Diffusion Equation');
-
-% Tridiagonal solver function
-function [x] = tri_diag_sol(a, b, c, d, b0, bL)
-    % Length of the system
-    J = length(a); 
-
-    % Adjust the first and last entries of d based on boundary conditions
-    d(1) = d(1) - b(1) * b0;  % Include left boundary condition
-    d(J) = d(J) - c(end) * bL;  % Include right boundary condition
-    
-    % Initialize alpha and delta
-    alpha = zeros(J, 1);
-    delta = zeros(J, 1);
-    alpha(1) = a(1);
-    delta(1) = d(1);
-
-    % Forward sweep for alpha and delta
+    d(1) = diagB(1)*FTCS(2) + sup_diagB(1)*FTCS(1);
     for j = 2:J
-        alpha(j) = a(j) - b(j-1)*c(j-1)/alpha(j-1);
-        delta(j) = d(j) - delta(j-1)*c(j-1)/alpha(j-1);
+        d(j) = sup_diagB(j-1)*FTCS(j-1) + diagB(j)*FTCS(j) + sub_diagB(j-1)*FTCS(j+1);
     end
+    d(J+1) = sub_diagB(J)*FTCS(J) + diagB(J+1)*FTCS(J+1);
     
-    % Back substitution for the solution
-    x = zeros(1, J); % Defining the solution vector
-    x(J) = delta(J)/alpha(J); % Last element
+    FTCSn = tri_diag_sol(a, b, c, d); 
 
-    % Solving for the rest of the elements
-    for j = J-1:-1:1
-        x(j) = (delta(j) - b(j)*x(j+1))/alpha(j);
+    if mod(n*dt,2) < dt % check if current time is close to a multiple of 2
+        hold on, plot(x,FTCSn,'b'), hold off % if so, add a plot of current solution to existing plot
     end
+
+    % Update for the next time step
+    FTCS = FTCSn;
 end
+
--- tri_diag_sol.m
+++ tri_diag_sol.m
@@ -1,17 +1,9 @@
-function [x] = tri_diag_sol(a,b,c,d,b0,bL)
-
+function [x] = tri_diag_sol(a,b,c,d)
     J = length(a); % could also be length(d)
 
-    % Adjust the first and last entries of d based on boundary conditions
-    d(1) = d(1) - b(1) * b0;  % Include left boundary condition
-    d(J) = d(J) - c(end) * bL;  % Include right boundary condition
-    
-    % Initialize alpha and delta
-    alpha = zeros(J, 1);
-    delta = zeros(J, 1);
     alpha(1) = a(1);
     delta(1) = d(1);
-
+    
     % defining the entire alpha and delta vector
     for j = 2:J
         alpha(j) = a(j) - b(j-1)*c(j-1)/alpha(j-1);
@@ -20,9 +12,9 @@ function [x] = tri_diag_sol(a,b,c,d,b0,bL)
     
     x = zeros(1, J); % defining the solution
     x(J) = delta(J)/alpha(J); % last x element
-
+    
     % solving for the x vector (the rest of the elements)
-    for j = J-1:-1:1
-        x(j) = (delta(j) - b(j)*x(j+1))/alpha(j);
+    for j = J:-1:2
+        x(j-1) = (delta(j-1) - b(j-1)*x(j))/alpha(j-1);
     end
 end
